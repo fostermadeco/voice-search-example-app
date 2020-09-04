86cb9a4f9532992a6154f8ed8748b98066b214e2
Kelly Packer <kelly@kellypacker.com>
2020-09-04 11:55:57 -0600

    Convert to function component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ActivityIndicator } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import FadeInView from './components/FadeInView';
@@ -40,34 +40,29 @@ const recordingOptions = {
     },
 };
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.recording = null;
-        this.state = {
-            isFetching: false,
-            isRecording: false,
-            query: '',
-        }
-    }
+const App = () => {
+    const [recording, setRecording] = useState(null);
+    const [isFetching, setIsFetching] = useState(false);
+    const [isRecording, setIsRecording] = useState(false);
+    const [query, setQuery] = useState('');
 
-    async componentDidMount() {
-        const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
-    }
+    useEffect(() => {
+        Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    }, []);
 
-    deleteRecordingFile = async () => {
+    const deleteRecordingFile = async () => {
         try {
-            const info = await FileSystem.getInfoAsync(this.recording.getURI());
+            const info = await FileSystem.getInfoAsync(recording.getURI());
             await FileSystem.deleteAsync(info.uri)
         } catch(error) {
             console.log("There was an error deleting recording file", error);
         }
     }
 
-    getTranscription = async () => {
-        this.setState({ isFetching: true });
+    const getTranscription = async () => {
+        setIsFetching(true);
         try {
-            const info = await FileSystem.getInfoAsync(this.recording.getURI());
+            const info = await FileSystem.getInfoAsync(recording.getURI());
             console.log(`FILE INFO: ${JSON.stringify(info)}`);
             const uri = info.uri;
             const formData = new FormData();
@@ -82,20 +77,20 @@ export default class App extends React.Component {
             });
             const data = await response.json();
             console.log(data);
-            this.setState({ query: data.transcript });
+            setQuery(data.transcript);
         } catch(error) {
             console.log('There was an error reading file', error);
-            this.stopRecording();
-            this.resetRecording();
+            stopRecording();
+            resetRecording();
         }
-        this.setState({ isFetching: false });
+        setIsFetching(false);
     }
 
-    startRecording = async () => {
+    const startRecording = async () => {
         const { status } = await Permissions.getAsync(Permissions.AUDIO_RECORDING);
         if (status !== 'granted') return;
 
-        this.setState({ isRecording: true });
+        setIsRecording(true);
         await Audio.setAudioModeAsync({
             allowsRecordingIOS: true,
             interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
@@ -111,78 +106,72 @@ export default class App extends React.Component {
             await recording.startAsync();
         } catch (error) {
             console.log(error);
-            this.stopRecording();
+            stopRecording();
         }
 
-        this.recording = recording;
+        setRecording(recording);
     }
 
-    stopRecording = async () => {
-        this.setState({ isRecording: false });
+    const stopRecording = async () => {
+        setIsRecording(false);
         try {
-            await this.recording.stopAndUnloadAsync();
+            await recording.stopAndUnloadAsync();
         } catch (error) {
             // Do nothing -- we are already unloaded.
         }
     }
 
-    resetRecording = () => {
-        this.deleteRecordingFile();
-        this.recording = null;
-    }
+    const resetRecording = () => {
+        deleteRecordingFile();
+        setRecording(null);
+    };
 
-    handleOnPressIn = () => {
-        this.startRecording();
-    }
+    const handleOnPressIn = () => {
+        startRecording();
+    };
 
-    handleOnPressOut = () => {
-        this.stopRecording();
-        this.getTranscription();
-    }
+    const handleOnPressOut = () => {
+        stopRecording();
+        getTranscription();
+    };
 
-    handlQueryChange = (query) => {
-        this.setState({ query });
-    }
+    const handleQueryChange = (query) => {
+        setQuery(query);
+    };
 
-
-
-    render() {
-        const { isRecording, query, isFetching } = this.state;
-        return (
-            <SafeAreaView style={{flex: 1}}>
-                <View style={styles.container}>
-                    {isRecording &&
-                        <FadeInView>
-                            <FontAwesome name="microphone" size={32} color="#48C9B0" />
-                        </FadeInView>
-                    }
-                    {!isRecording &&
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <View style={styles.container}>
+                {isRecording &&
+                    <FadeInView>
                         <FontAwesome name="microphone" size={32} color="#48C9B0" />
-                    }
-                    <Text>Voice Search</Text>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPressIn={this.handleOnPressIn}
-                        onPressOut={this.handleOnPressOut}
-                    >
-                        {isFetching && <ActivityIndicator color="#ffffff" />}
-                        {!isFetching && <Text>Hold for Voice Search</Text>}
-                    </TouchableOpacity>
-                </View>
-                <View style={{paddingHorizontal: 20}}>
-                    <InstantSearch
-
-                        indexName={config.ALGOLIA_INDEX}
-                        searchClient={searchClient}
-                    >
-                        <SearchBox query={query} onChange={this.handlQueryChange} />
-                        <Hits />
-                    </InstantSearch>
-                </View>
-            </SafeAreaView>
-        );
-    }
-}
+                    </FadeInView>
+                }
+                {!isRecording &&
+                    <FontAwesome name="microphone" size={32} color="#48C9B0" />
+                }
+                <Text>Voice Search</Text>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPressIn={handleOnPressIn}
+                    onPressOut={handleOnPressOut}
+                >
+                    {isFetching && <ActivityIndicator color="#ffffff" />}
+                    {!isFetching && <Text>Hold for Voice Search</Text>}
+                </TouchableOpacity>
+            </View>
+            <View style={{paddingHorizontal: 20}}>
+                <InstantSearch
+                    indexName={config.ALGOLIA_INDEX}
+                    searchClient={searchClient}
+                >
+                    <SearchBox query={query} onChange={handleQueryChange} />
+                    <Hits />
+                </InstantSearch>
+            </View>
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -199,3 +188,5 @@ const styles = StyleSheet.create({
         marginTop: 20,
     }
 });
+
+export default App;
